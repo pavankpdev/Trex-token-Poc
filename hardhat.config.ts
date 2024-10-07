28b82d2d510037494321bc9fb82dd0927d4abcbd
pavanleton <pavankumarkpkp@gmail.com>
2024-10-07 11:32:37 +0530

    modify hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,7 @@ import '@openzeppelin/hardhat-upgrades';
 import 'solidity-coverage';
 import '@nomiclabs/hardhat-solhint';
 import '@primitivefi/hardhat-dodoc';
+import 'hardhat-deploy';
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -19,11 +20,26 @@ const config: HardhatUserConfig = {
   gasReporter: {
     enabled: true,
   },
+  networks: {
+    local: {
+      url: 'HTTP://127.0.0.1:7545',
+      accounts: ["0x1538d25d04e53f4bc73e1aef6dff73d4e34952440385f1cb93d3515e50099e10"],
+    },
+
+  },
   dodoc: {
     runOnCompile: false,
     debugMode: true,
     outputDir: "./docgen",
     freshOutput: true,
+  },
+  paths: {
+    deploy: 'deploy',
+    deployments: 'deployments',
+    imports: 'imports'
+  },
+  namedAccounts: {
+    deployer: 0,
   },
 };
 
